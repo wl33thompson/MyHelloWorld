22f6c852c87113c92e686ff9f0042a19f3af4c52
Thompson <wl33thompson@gmail.com>
2019-12-08 20:02:38 +0800

    second commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//package com.example.springbootproj.controller;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
